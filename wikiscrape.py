65fffa7e888d668bbe039a54b7693620c10d1531
vahgon <mullins.ian@outlook.com>
2026-01-13 23:49:58 -0500

    start creating regex for determining column content in regards to the specific fsig row

--- wikiscrape.py
+++ wikiscrape.py
@@ -48,28 +48,59 @@ def make_request():
 
     format_response(res)
 
-def format_response(res: Response):
-    from bs4 import BeautifulSoup                                      # noqa: E402
-    soup  = BeautifulSoup(markup=res.text, features='lxml')
-    table = soup.select_one('table[class~=wikitable]')
+def pop_row(rows: bs4.ResultSet[bs4.Tag]):
+    rows[0].decompose
+    _ = rows.pop(0)
 
-    if table is None:
-        raise MissingContentError(f"Request to '{wikimedia_api_call}' did not return a table of class 'wikitable'")
+is_hex          = re.compile(pattern=r'(([A-F0-9?]{2})+\s)+([A-F0-9]{2})$')
+is_iso          = re.compile(pattern=r'')
+is_offset       = re.compile(pattern=r'',                                       flags=re.IGNORECASE)
+is_extension    = re.compile(pattern=r'',                                       flags=re.IGNORECASE)
+is_desc         = re.compile(pattern=r'',                                       flags=re.IGNORECASE)
+is_l_end_hex    = re.compile(pattern=r'little-endian(?!.*big-endian)',          flags=re.IGNORECASE)
+is_b_end_hex    = re.compile(pattern=r'big-endian(?!.*little-endian)',          flags=re.IGNORECASE)
 
-    rows = List[Row]
+def find_content(data: bs4.Tag | None) -> str | None:
+    if data is None:
+        raise NoTagFound(f'No tag found for {data}')
+    if data.is_empty_element:
+        return
 
-    for r in table.css.select('tr'):
-        children = r.find_all()
-        row: Row.columns = []
-        for child in children:
-            col: Column = { 'tag_name': str(type(child)), 'tag': child, 'content': child.text }
-            row.append(col)
+    if re.search(pattern=is_hex, string=data.get_text()):
+        return 'hex'
 
-            #if child.name == 'code':
-                #print(child)
-            #elif child.name != 'th':
-                #print(child)
-                #pass
+def format_table(rows: bs4.ResultSet[bs4.Tag]) -> None:
+    table: dict[str, str | None] = {}
+
+    for header in rows[0]:
+        table[header.get_text()] = None
+    pop_row(rows)
+
+    while True:
+        if len(rows) >= 1:
+            for col in rows[0]:
+                match find_content(col.find_next()):
+                    case 'hex':
+                        print(col)
+                    case None:
+                        pass
+                    case _:
+                        raise ElementError(f'Default case for {col.get_text} (this shouldn\'t happen)')
+
+            pop_row(rows)
+        else:
+            break
+
+def format_response(res: requests.Response):
+    from bs4.filter import SoupStrainer
+    table = bs4.BeautifulSoup(markup=res.text.replace('\n', ''), parse_only=SoupStrainer('table',{ 'class': 'wikitable sortable' }), features='lxml')
+    table_body = table.find('tbody', recursive=True)
+
+    if not isinstance(table_body, bs4.Tag):
+        raise NoTagFound('GET response did not return a table with a <tbody></tbody> tags')
+
+    rows = table_body.find_all('tr')
+    format_table(rows)
 
 if __name__ == "__main__":
     res = make_request()
