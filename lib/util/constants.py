9967773888940596f884df94615a1429d3387b9f
vahgon <mullins.ian@outlook.com>
2026-01-26 12:34:57 -0500

    Moved type aliases, UserArguments, new path constants to constants.py. File moved to lib/util

---
+++ lib/util/constants.py
@@ -0,0 +1,29 @@
+from lib.util._args import UserArgument
+from pathlib import Path
+from enum import Enum
+import re
+
+USER_ARGS: UserArgument = UserArgument()
+type HtmlJson = dict[str, str | int]
+type ReqJson = dict[str, HtmlJson]
+type TableHeaders = list[str]
+type TableRows = list[dict[str, str | None]]
+
+ColType = Enum('ColType', [('HEX', 1), ('ISO', 2), ('OFF', 3), ('EXT', 4), ('INF', 5)])
+
+ROOT: Path = Path(__file__).parent
+DOCS_PATH: Path = Path(f'{Path(__file__).parent.parent.parent}/docs')
+ENV_PATH: Path = Path(f'{ROOT}/.conf')
+
+FSIG_REGEX = {
+    'HEX_REGEX': re.compile(pattern=r'(([A-F0-9?]{2})(\s{1})?)+',flags=re.IGNORECASE),
+    'CONTAINER_REGEX': re.compile(pattern=r'([A-F0-9?]{2}\s?)+(\(([\w\s-])*\))(([A-F0-9?]{2}\s?)+(\(([\w\s-])*\)))',flags=re.IGNORECASE),
+    'LITTLE_ENDIAN_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
+    'BIG_ENDIAN_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
+    'BIG_LITTLE_ENDIAN_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
+    'WEIRD_CHARS_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
+    'ISO_REGEX': re.compile(pattern=r''),
+    'OFFSET_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
+    'EXTENSION_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
+    'DESC_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE)
+}
