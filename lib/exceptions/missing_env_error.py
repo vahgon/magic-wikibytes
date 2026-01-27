65f394d6cb399df4a89126b39d75479daeec6739
vahgon <mullins.ian@outlook.com>
2026-01-27 10:48:04 -0500

    Added new exception that is raised when dotenv value is empty

--- lib/exceptions/missing_env_error.py
+++
@@ -1,7 +0,0 @@
-from lib.exceptions.wikibytes_error import WikibytesError
-
-class MissingEnvError(WikibytesError):
-    """
-    This exception is raised when a value could not be read from .env
-    """
-    pass
---
+++ lib/exceptions/missing_envar_error.py
@@ -0,0 +1,7 @@
+from lib.exceptions.wikibytes_error import WikibytesError
+
+class MissingEnvVarError(WikibytesError):
+    """
+    This exception is raised when a variable could not be found in .conf
+    """
+    pass
