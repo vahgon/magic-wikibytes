61f1a6b1044b35e550a66f67aac4fa9112a0d2f1
vahgon <mullins.ian@outlook.com>
2026-02-09 00:42:28 -0500

    Improved table formatting implementation

--- lib/parser/_wikitable_parser.py
+++ lib/parser/_wikitable_parser.py
@@ -1,5 +1,7 @@
+from lib.util import ColType, FileData, USER_ARGS
 from lib.exceptions import NoTagFoundError, WikitableFormatError
 from collections import defaultdict
+from codecs import encode, decode
 import re
 
 try:
@@ -9,43 +11,124 @@ except (ImportError, ModuleNotFoundError) as e:
     e.add_note("bs4 not found. Install with -> pip install bs4")
     raise e
 
-newTable: defaultdict[int, ResultSet[Tag]] = defaultdict(lambda: ResultSet(source=None, result=[Tag(name='') for _ in range(5)]))
-def _format_wikitable(wikitable: Tag) -> None:
+wikitable: Tag | BeautifulSoup | None
+parser: SoupStrainer = SoupStrainer(name='table', attrs={ 'class': 'wikitable sortable' })
+
+parsedTable: defaultdict[int, ResultSet[Tag]] = defaultdict(lambda: ResultSet(source=None, result=[Tag(name='') for _ in range(5)]))
+listTable: list[dict[str, FileData]] = list()
+
+def _format_iso(isoText: str) -> str:
+    ...
+
+def _format_table_opts() -> None:
+    ...
+
+def _format_hex(hexText: str) -> str:
+    qGroup: re.Match[str] | None = None
+    for byteGroup in range(len(re.findall(pattern=r'(\?{2})+', string=hexText))):
+        qGroup = re.search(pattern=r'(?:\?{2})+', string=hexText)
+    if qGroup:
+        qByteAmount = qGroup.group(0).count('??')
+        hexText = str(hexText[:qGroup.start()] + hexText[qGroup.end():])
+
+    hex = bytes.fromhex(hexText)
+    return str(hex.hex(sep=' '))
+
+def _mk_file_dict() -> None:
+    global listTable
+
+    for row in parsedTable:
+        fileSig: dict[str, FileData] = dict()
+        for idx, col in enumerate(parsedTable[row]):
+            match idx:
+                case ColType.HEX:
+                    hex: list[str | bytes] = list()
+                    for codeTag in col.find_all('code'):
+                        hex.append(_format_hex(codeTag.extract().get_text().replace(' ', '').replace('\xa0', '')))
+                        # if hex != "":
+                        #     hex += '\n'+_format_hex(codeTag.extract().get_text().replace(' ', '').replace('\xa0', ''))
+                        # else:
+                        #     hex = _format_hex(codeTag.extract().get_text().replace(' ', '').replace('\xa0', ''))
+
+                    fileSig[col.name] = hex
+                case ColType.ISO:
+                    fileSig[col.name] = col.get_text()
+                case ColType.OFF:
+                    fileSig[col.name] = col.get_text()
+                case ColType.EXT:
+                    fileSig[col.name] = col.get_text()
+                case ColType.DES:
+                    fileSig[col.name] = col.get_text()
+                case _:
+                    raise WikitableFormatError()
+        listTable.append(fileSig)
+
+def _format_tag_children(col: Tag) -> None:
+    badTags = ['cite', 'sup', 'span']
+    fixTags = ['a']
+
+    for tag in badTags:
+        for colTag in col.find_all(tag):
+            colTag.decompose()
+
+    for tag in fixTags:
+        for childTag in col.find_all(tag):
+            _ = childTag.replaceWithChildren()
+
+    for codeTag in col.find_all('code'):
+        for br in codeTag.find_all('br'):
+            _ = br.extract()
+
+def _parse_wikitable(wikitable: Tag) -> None:
+    global parsedTable
     rows = [row.find_all(['td', 'th']) for row in wikitable.select('tr')]
     headers = [re.sub(pattern=r'\n', repl='', string=x.extract().get_text()) for x in rows.pop(0)[:]]
 
-    global newTable
+    if len(headers) != 5:
+        e = WikitableFormatError()
+        e.add_note("Problem initializing wikitable parser - could not find the expected 5 <th> tags")
+        raise e
+
     for rowIdx, row in enumerate(rows):
         for colIdx, col in enumerate(row):
-            if 'rowspan' in col.attrs:
+            _format_tag_children(col)
+            if col.has_attr('rowspan'):
                 for i in range(int(col.attrs['rowspan'][0])):
-                    newTable[rowIdx + i][colIdx] = col
+                    parsedTable[rowIdx + i][colIdx] = col
+                    parsedTable[rowIdx + i][colIdx].name = headers[colIdx]
                 del col.attrs['rowspan']
                 continue
             if len(row) != 5:
-                for i in range(len(newTable[rowIdx])):
-                    if newTable[rowIdx][i].name == '':
-                        newTable[rowIdx][i] = col
+                for i in range(len(row)):
+                    if parsedTable[rowIdx][i].name == '':
+                        parsedTable[rowIdx][i] = col
+                        parsedTable[rowIdx][i].name = headers[i]
                         break
                 continue
-            newTable[rowIdx][colIdx] = col
-    for e in newTable:
-        for i in newTable[e]:
-            print(i.get_text())
-        
-        input()
-        print()
+            parsedTable[rowIdx][colIdx] = col
+            parsedTable[rowIdx][colIdx].name = headers[colIdx]
+    _mk_file_dict()
 
-def parse_html(html: str) -> None:
-    parser = SoupStrainer(name='table', attrs={ 'class': 'wikitable sortable' })
-    wikitable = BeautifulSoup(markup=html, parse_only=parser, features='lxml').select_one('tbody')
+def parse_html(html: str) -> list[dict[str, FileData]]:
+    global wikitable
+    wikitable = BeautifulSoup(markup=re.sub(pattern=r'\n', repl='', string=html), parse_only=parser, features='lxml').select_one('tbody')
 
     if not (isinstance(wikitable, Tag)):
         e = NoTagFoundError()
         e.add_note("Error formatting wikitable - no <tbody> tag was found.")
         raise e
 
-    _ = _format_wikitable(wikitable)
+    _parse_wikitable(wikitable)
+    return listTable
 
 def pretty_html(html: str) -> str:
-    ...
+    global wikitable
+    if not isinstance(wikitable, Tag):
+        wikitable = BeautifulSoup(markup=html, parser=parser, features='lxml')
+
+    if not wikitable:
+        e = WikitableFormatError()
+        e.add_note("Error formatting wikitable - no <tbody> tag was found.")
+        raise e
+    
+    return wikitable.prettify()
--- lib/parser/table.py
+++ lib/parser/table.py
@@ -1,86 +1,76 @@
-from lib.parser._wikitable_parser import parse_html
+from lib.parser._wikitable_parser import parse_html, pretty_html
+from lib.util import DOCS_PATH, USER_ARGS, FileData
 from lib.parser._html_obj import HTML
-from lib.util import DOCS_PATH, USER_ARGS
 from pathlib import Path
+import logging
 
 try:
     from pandas import DataFrame
 except (ImportError, ModuleNotFoundError) as e:
-    e.add_note("pandas not found. Install with-> pip install pandas")
+    e.add_note("pandas not found. Install with -> pip install pandas")
     raise e
 
 class Table(HTML):
     def __init__(self) -> None:
         super().__init__()
-        self.rawFileSigs: list[dict[str, str | None]] = list()
+        self.fileOutput: Path | None = USER_ARGS.output
+        self.fileFormat: str | None = USER_ARGS.format
+        self.fileToDocs: bool | None = USER_ARGS.docs
+
+        self.rawFileSigs: list[dict[str, FileData]]
         self.df: DataFrame = DataFrame()
-        self.verboseStr: str = ''
 
-        self.format: str | None = USER_ARGS.format
-        self.output: Path | None = USER_ARGS.output
+    def _determine_io(self) -> None:
+        if self.fileOutput:
+            logging.info(f'Creating file {self.fileOutput}')
+            self._set_output(self.fileOutput)
 
-    def _print_or_create(self) -> None:
-        if self.output and self.format:
-            self.output = Path(f'{self.output}.{self.format}')
-            self.verboseStr = f'Both output and format given - Created file {self.output}'
-            self._set_output(self.output)
+        elif self.fileFormat:
+            logging.info(f'Printing wikitable to screen as {self.fileFormat}')
+            self._set_output(self.fileFormat)
 
-        elif self.output:
-            self.verboseStr = f'Created file {self.output}'
-            self._set_output(self.output)
-
-        elif self.format:
-            self.verboseStr = f'Table printed to console as {self.format}'
-            self._set_output(self.format)
-
-        else:
-            self.output = Path(f'{DOCS_PATH}/signatures.*')
-            self.verboseStr = f'No flags set for output or format - defaulted to {self.output}'
+        elif self.fileToDocs:
+            logging.info(f'Creating wikitable in all implemented formats and saving to {Path(f'{DOCS_PATH}/signatures.*')}')
             self._set_output(None)
-
-        if USER_ARGS.verbose:
-            print(self.verboseStr)
+        else:
+            logging.info("No file format or output path provided, Printing raw response data.")
+            print(pretty_html(self.html))
 
     def _set_output(self, out: Path | str | None) -> None:
         suffix: str | None = out.suffix if isinstance(out, Path) else out 
-        toPrint: str | None
 
         match suffix:
             case '.json' | 'json':
-                toPrint = self._create_json()
+                _ = self._create_json()
             case '.csv' | 'csv':
-                toPrint = self._create_csv()
+                _ = self._create_csv()
             case '.md' | 'md':
-                toPrint = self._create_md()
+                _ = self._create_md()
             case None:
-                self.output = Path(f'{DOCS_PATH}/signatures.json')
-                toPrint = self._create_json()
-
-                self.output = Path(f'{DOCS_PATH}/signatures.csv')
-                toPrint = self._create_csv()
-
-                self.output = Path(f'{DOCS_PATH}/signatures.md')
-                toPrint = self._create_md()
+                self.fileOutput = Path(f'{DOCS_PATH}/signatures.json')
+                _ = self._create_json()
+                self.fileOutput = Path(f'{DOCS_PATH}/signatures.csv')
+                _ = self._create_csv()
+                self.fileOutput = Path(f'{DOCS_PATH}/signatures.md')
+                _ = self._create_md()
             case _:
                 e = Exception()
                 raise e
 
-        if isinstance(toPrint, str):
-            print(toPrint)
-
     def _create_json(self) -> str | None:
-        return self.df.to_json(path_or_buf=self.output, lines=False, orient='index', indent=4, force_ascii=False)
+        self.df = DataFrame(self.rawFileSigs)
+        return self.df.to_json(self.fileOutput, orient='index', indent=True, force_ascii=False)
 
     def _create_csv(self) -> str | None:
-        return self.df.to_csv(path_or_buf=self.output)
+        self.df = DataFrame(self.rawFileSigs)
+        return self.df.to_csv(path_or_buf=self.fileOutput)
 
     def _create_md(self) -> str | None:
-        md = self.df.replace('\n', '<br>', regex=True)
-        return md.to_markdown(self.output, tablefmt='github')
+        self.df = DataFrame(self.rawFileSigs)
+        return self.df.replace(r'\n', '<br>', regex=True).to_markdown(buf=self.fileOutput, tablefmt='github')
 
     def make_table(self) -> None:
-        self.rawFileSigs= parse_html(self.html)
-        self.df = DataFrame(self.rawFileSigs)
-        self.df.fillna('',inplace=True)
+        self.rawFileSigs = parse_html(self.html)
+        self.df.fillna('', inplace=True)
 
-        self._print_or_create()
+        self._determine_io()
