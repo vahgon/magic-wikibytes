6e6992e279cde8ea20e34dadb61d0107884f09a8
vahgon <mullins.ian@outlook.com>
2026-02-09 00:38:04 -0500

    misc

--- lib/exceptions/unexpected_format_error.py
+++ lib/exceptions/unexpected_format_error.py
@@ -1,8 +1,8 @@
 from lib.exceptions.wikibytes_error import WikibytesError
 
 class UnexpectedFormatError(WikibytesError):
-    """
+    '''
     This exception is raised if the response from the request object sent to the wikimedia API returns data
     that is of an unexpected class when calling .json()
-    """
+    '''
     pass
