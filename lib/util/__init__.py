ce2c884cce939a303faa190301e7a1c06df47902
vahgon <mullins.ian@outlook.com>
2026-02-09 00:41:13 -0500

    Removed unneeded/unused constants. Changed var names to better reflect purpose

--- lib/util/__init__.py
+++ lib/util/__init__.py
@@ -1,13 +1,14 @@
+from lib.util._logger import log
+
 from lib.util.constants import(
-    ReqJson,        # noqa: F401
-    HtmlJson,       # noqa: F401
-    TableHeaders,   # noqa: F401
-    TableRows,      # noqa: F401
-    ColType,        # noqa: F401
-    ROOT,           # noqa: F401
-    USER_ARGS,      # noqa: F401
-    DOCS_PATH,      # noqa: F401
-    FSIG_REGEX,     # noqa: F401
+    ReqJson,            # noqa: F401
+    HtmlJson,           # noqa: F401
+    ColType,            # noqa: F401
+    FileData,            # noqa: F401
+    USER_ARGS,          # noqa: F401
+    DOCS_PATH,          # noqa: F401
+    ROOT,               # noqa: F401
 )
+
 from lib.util._set_env import get_email
 EMAIL: str = get_email()
--- lib/util/constants.py
+++ lib/util/constants.py
@@ -1,31 +1,21 @@
-from argparse import Namespace
 from lib.util._args import parse_args
+from argparse import Namespace
 from pathlib import Path
-from enum import Enum
-import re
+from enum import IntEnum
 
 USER_ARGS: Namespace = parse_args()
 
 type HtmlJson = dict[str, str | int]
 type ReqJson = dict[str, HtmlJson]
-type TableHeaders = list[str]
-type TableRows = list[dict[str, str | None]]
+type FileData = list[str | bytes] | str | int | bytes
 
-ColType = Enum('ColType', [('HEX', 1), ('ISO', 2), ('OFF', 3), ('EXT', 4), ('INF', 5)])
+class ColType(IntEnum):
+    HEX = 0
+    ISO = 1
+    OFF = 2
+    EXT = 3
+    DES = 4
 
 ROOT: Path = Path(__file__).parent
 DOCS_PATH: Path = Path(f'{Path(__file__).parent.parent.parent}/docs')
 ENV_PATH: Path = Path(f'{ROOT}/.conf')
-
-FSIG_REGEX = {
-    'HEX_REGEX': re.compile(pattern=r'(([A-F0-9?]{2})(\s{1})?)+',flags=re.IGNORECASE),
-    'CONTAINER_REGEX': re.compile(pattern=r'([A-F0-9?]{2}\s?)+(\(([\w\s-])*\))(([A-F0-9?]{2}\s?)+(\(([\w\s-])*\)))',flags=re.IGNORECASE),
-    'LITTLE_ENDIAN_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
-    'BIG_ENDIAN_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
-    'BIG_LITTLE_ENDIAN_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
-    'WEIRD_CHARS_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
-    'ISO_REGEX': re.compile(pattern=r''),
-    'OFFSET_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
-    'EXTENSION_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE),
-    'DESC_REGEX': re.compile(pattern=r'',flags=re.IGNORECASE)
-}
