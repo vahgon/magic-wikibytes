4087926162b3cca854f96d174ef754e64b7024b6
vahgon <mullins.ian@outlook.com>
2026-02-09 00:39:56 -0500

    Remove unused base class definitions; add var for wikipage Revision ID

--- lib/parser/_html_obj.py
+++ lib/parser/_html_obj.py
@@ -1,17 +1,11 @@
-from lib.get_request import get_json_response
+from lib.parser._get_request import get_response
 from lib.util import EMAIL
 
 class HTML:
     def __init__(self) -> None:
+        self.html: str
+        self.revId: int
         self.email: str = EMAIL
         self.resJson: dict[str, str | int]
-        self.html: str
 
-        [self.resJson, self.html] = get_json_response()
-
-    def compare_revision(self) -> None:
-        ...
-    def mk_markdown_table(self) -> None:
-        ...
-    def mk_json(self) -> None:
-        ...
+        [self.resJson, self.html, self.revId] = get_response()
