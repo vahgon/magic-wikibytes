113c8eed3bfb3ca07b4fa70f4112d5e18b6e6b50
vah <mullins.ian@outlook.com>
2026-02-16 23:46:44 -0500

    Added mutually exclusive groups; new arguments

--- lib/util/_args.py
+++ lib/util/_args.py
@@ -12,18 +12,28 @@ def parse_args(u_args: list[str] = sys.argv[1:]) -> Namespace:
                             in requests made to the wikipedia API. you can read why this should be set
                             at \"https://en.wikipedia.org/wiki/Special:RestSandbox/wmf-restbase\"""")
 
-    _ = parser.add_argument('-v', '--verbose', required=False, action='store_true',
-                            help="""print actions as they are taken during script execution""")
+    determineOutput = parser.add_mutually_exclusive_group()
 
-    _ = parser.add_argument('-o', '--output', required=False, type=Path, metavar='', default=None,
+    _ = determineOutput.add_argument('-o', '--output', required=False, type=Path, metavar='', default=None,
                             help="""specify the output file. if none specified, output will be 
-                            printed to console""")
+                            printed to console. default filetype is json if none is included.""")
 
-    _ = parser.add_argument('-f', '--format', required=False, type=str, metavar='', default=None, choices=['json','csv','md'],
+    _ = determineOutput.add_argument('-f', '--format', required=False, type=str, metavar='', default=None, choices=['json','csv','md'],
                             help="""prints output to the terminal in the format specified by input
                             given (e.g. -e json)""")
 
-    _ = parser.add_argument('-q', '--quiet', required=False, default=False, action='store_true',
+    _ = determineOutput.add_argument('-d', '--docs', required=False, action='store_true',
+                            help="""will create all supported filetypes & send to docs/""")
+
+    _ = parser.add_argument('--hex', required=False, type=str, metavar='', default=None, choices=['prefix'],
+                            help="""set hex output format""")
+
+    terminalVolume = parser.add_mutually_exclusive_group()
+
+    _ = terminalVolume.add_argument('-v', '--verbose', required=False, action='store_true',
+                            help="""print actions as they are taken during script execution""")
+
+    _ = terminalVolume.add_argument('-q', '--quiet', required=False, default=False, action='store_true',
                             help="""supress all messages that would usually appear
                             during %(prog)s's execution.""")
 
