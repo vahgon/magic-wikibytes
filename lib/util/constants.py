595f1e54698d2645b5f360dc1d30296d8a525e74
vahgon <mullins.ian@outlook.com>
2026-03-03 23:35:27 -0500

    Add tag lists to constants.py

--- lib/util/constants.py
+++ lib/util/constants.py
@@ -1,13 +1,16 @@
-from lib.util._args import parse_args
 from argparse import Namespace
-from pathlib import Path
 from enum import IntEnum
+from pathlib import Path
+from typing import Self
+
+from lib.util._args import parse_args
 
 USER_ARGS: Namespace = parse_args()
 
 type HtmlJson = dict[str, str | int]
 type ReqJson = dict[str, HtmlJson]
-type FileData = list[str | bytes] | list[str] | str | int | bytes
+type FileData = (list[bytes] | list[str] | str | bytes | int)
+type ParsedTableDict = list[dict[str, FileData]]
 
 class ColType(IntEnum):
     HEX = 0
@@ -15,6 +18,9 @@ class ColType(IntEnum):
     OFF = 2
     EXT = 3
     DES = 4
+
+BADTAGS = ['cite', 'sup', 'span', 'br']
+FIXTAGS = ['a', 'p']
 
 HEADERNAMES = ['Hex Signature', 'ISO 8859-1', 'Offset', 'Extension', 'Description']
 
