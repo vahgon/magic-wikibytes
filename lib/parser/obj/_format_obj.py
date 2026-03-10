66b006be6d0f3262c3fc2a50a3b0170612b9b734
vahgon <mullins.ian@outlook.com>
2026-03-09 23:20:06 -0400

    Properly balances hex and iso wikitable columns

--- lib/parser/obj/_format_obj.py
+++ lib/parser/obj/_format_obj.py
@@ -1,26 +1,33 @@
-JNK_CHARS = ['\xa0', ' ']
+from dataclasses import dataclass, field
 
-class Filebyte:
-    def __init__(self, hexStr: str) -> None:
-        self.__text:        str       = hexStr
-        self.__byte_list:   list[str] = [self.__text[i:i+2] for i in range(0, len(self.__text), 2)]
+@dataclass
+class checkbytes:
+    '''
+    A checkbytes object checks the equality of the bytes in the provided hex and iso values.
+    It will first check to see if they are balanced - len(bytes in hex) == len(bytes in iso), 
+    then will make adjustments accordingly. 
+    '''
+    hex: str
+    iso: str
 
-    def format_hexstring(self) -> str:
-        hexString:  str = self.__text
+    _hex_to_latin1: str = field(default_factory=str)
 
-        for char in JNK_CHARS:
-            hexString = self.__text.replace(char, '')
-        return hexString
+    _h_bytes: (list[str] | list[list[str]]) = field(default_factory=list)
+    _i_bytes: (list[str] | list[list[str]]) = field(default_factory=list)
 
-class Latin1:
-    def __init__(self, hexStr: str) -> None:
-        self.sisterHex: str = hexStr
-        self.__latin1:  str   = ''
+    def __post__init__(self) -> None:
+        self._h_bytes = [self.hex[idx:idx+2] for idx in range(0, len(self.hex), 2)]
+        self._i_bytes = [byte for byte in self.iso]
 
-    def format_iso(self) -> str:
-        for idx in range(0, len(self.sisterHex), 2):
-            byte = self.sisterHex[idx:idx+2]
-            if byte != '??':
-                byte_hex = bytes.fromhex(byte)
-                self.__latin1 += byte_hex.decode('latin-1')
-        return self.__latin1
+    def is_equal(self) -> (bool | None):
+        '''
+        Checks if each given byte of the hex string is equal to its converted iso 8859-1
+        representation.
+        
+        :return: bool 
+        '''
+        self.__post__init__()
+
+        if len(self._h_bytes) != len(self._i_bytes):
+            return False
+        return True
