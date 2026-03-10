4e46c381852e3a9687801e17a8150dc99e3ce4f3
vahgon <mullins.ian@outlook.com>
2026-03-09 23:21:06 -0400

    Removed unused row responsibilities

--- lib/parser/obj/_row_containers.py
+++ lib/parser/obj/_row_containers.py
@@ -3,42 +3,20 @@ from typing import Self
 from bs4 import ResultSet, Tag
 
 from lib.parser.obj._col_containers import ColumnFactory, FileSignatureTag
-from lib.util.constants import ColType, FIXTAGS, BADTAGS, HEADERNAMES
-
-TAGS            = set(FIXTAGS) | set(BADTAGS)
-repl_children   = Tag.unwrap
-destroy         = Tag.decompose
-
-appendRow: Callable[[list[FileSignatureTag], FileSignatureTag], None] = list.append
 
 class Row:
     def __init__(self, row: ResultSet[Tag]) -> None:
-        self._cols:         list[FileSignatureTag]  = []
-        self._colAmount:    int                     = len(row)
-        self.__idx:         int                     = 0
-        self.row:           ResultSet[Tag]          = row
-
-        self._fill_row()
-
-    def _format_children(self, col: Tag) -> Tag:
-        for tag in col.find_all(TAGS, recursive=True):
-            if tag in BADTAGS:
-                destroy(tag)
-            elif tag in FIXTAGS:
-                _ = repl_children(tag)
-        return col
-
-    def _fill_row(self) -> None:
-        for col in ColType:
-            appendRow(self._cols, ColumnFactory.col_type(col, self._format_children(self.row[col]), self.row))
-            self._cols[col].name = HEADERNAMES[col]
+        self._cols:       list[FileSignatureTag] = ColumnFactory().set_row(row)
+        self._col_amount: int                    = len(row)
+        self.__idx:       int                    = 0
+        self.row:         ResultSet[Tag]         = row
 
     def __iter__(self) -> Self:
         self.__idx = 0
         return self
 
     def __next__(self) -> FileSignatureTag:
-        if self.__idx < self._colAmount:
+        if self.__idx < self._col_amount:
             result = self._cols[self.__idx]
             self.__idx += 1
             return result
