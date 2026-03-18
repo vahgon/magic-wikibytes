80473ef5067fd9f3f101e3705932a1b23ee7520d
vahgon <mullins.ian@outlook.com>
2026-03-17 23:39:46 -0400

    Remove csv from choices arg

--- lib/util/_args.py
+++ lib/util/_args.py
@@ -47,27 +47,36 @@ def parse_args(u_args: list[str] = sys.argv[1:]) -> UserArguments:
     parser.add_argument('-o', '--output', required=False, type=str, metavar='', default='.',
                         help="""Specify the output file. if none specified, output will be printed
                         to console. default filetype is json if none is included.""")
-    parser.add_argument('-f', '--format', required=False, type=str, metavar='', default=None, choices=['json', 'csv', 'md', 'all'],
+
+    parser.add_argument('-f', '--format', required=False, type=str, metavar='', default=None, choices=['json', 'md', 'all'],
                         help="""Prints output to the terminal in the format specified by input given (e.g. -e json).""")
+
     parser.add_argument('--hex-separator', required=False, type=str, metavar='', default='',
                         help="""Specify character/string to use to separate hexadecimal bytes (default = '').""")
+
     parser.add_argument('--wildcard-char', required=False, type=str, metavar='', default='?',
                         help="""Specify a character to act as found wildcard bytes (default='?')""")
+
     parser.add_argument('--rowspan-newrow', required=False, action='store_true',
                         help="""Succeeding rows affected by rowspan attributes in the current row's
                         columns will have their own instances when parsed instead of being appended to
                         the current row.""")
+
     parser.add_argument('--ext-parens', required=False, action='store_true',
                         help="""Remove parenthesis found in file signature extension(s).""")
+
     parser.add_argument('--no-bigend', required=False, action='store_true',
                         help="""Ignore all big-endian formatted rows from the wikitable.""")
+
     parser.add_argument('--force-latin1', required=False, action='store_true',
                         help="""Force the decoding of all hexadecimal values to latin-1 instead of
                         only unbalanced iso-8559 and hexadecimal columns.""")
 
     terminal_vol = parser.add_mutually_exclusive_group()
+
     terminal_vol.add_argument('--verbose', required=False, action='store_true',
                         help="""Print actions as they are taken during script execution.""")
+
     terminal_vol.add_argument('--quiet', required=False, default=False, action='store_true',
                         help="""Supress all messages that would usually appear during %(prog)s's execution.""")
 
